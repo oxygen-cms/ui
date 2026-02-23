12419bb36da6edade4d4cc80c1c9246288008e03
Chris Chamberlain <chris@chamberlain.id.au>
2026-02-23 17:31:50 +1100

    update preferences

--- src/components/preferences/UserPreferences.vue
+++ src/components/preferences/UserPreferences.vue
@@ -1,10 +1,7 @@
 <template>
     <div>
-        <h3 class="subtitle">General</h3>
-        <PreferencesField label="Font Size" data-key="user.general::fontSize" type="select" default-text="group"></PreferencesField>
         <h3 class="subtitle">Editor</h3>
         <PreferencesField label="Default Mode" data-key="user.editor::defaultMode" type="select" default-text="group"></PreferencesField>
-        <PreferencesField label="Overall Theme" data-key="user.editor::theme" type="select" default-text="group"></PreferencesField>
         <h4 class="subtitle is-6">Code Editor</h4>
         <PreferencesField label="Theme" data-key="user.editor::ace.theme" type="select" grouped default-text="group"></PreferencesField>
         <PreferencesField label="Font Size" data-key="user.editor::ace.fontSize" type="select" default-text="group"></PreferencesField>
@@ -12,8 +9,6 @@
         <PreferencesField label="Highlight Active Line" data-key="user.editor::ace.highlightActiveLine" type="switch" default-text="group"></PreferencesField>
         <PreferencesField label="Show Print Margin" data-key="user.editor::ace.showPrintMargin" type="switch" default-text="group"></PreferencesField>
         <PreferencesField label="Show Invisibles" data-key="user.editor::ace.showInvisibles" type="switch" default-text="group"></PreferencesField>
-        <h4 class="subtitle is-6">Design Editor</h4>
-        <PreferencesField label="Theme" data-key="user.editor::ckeditor.skin" type="select" grouped default-text="group"></PreferencesField>
     </div>
 </template>
 
